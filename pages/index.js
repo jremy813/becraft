54dc91478f2e5e9dd3fa65cb65aca9519ddd90f2
jremy813 <jremywatkins@gmail.com>
2020-08-02 14:09:39 -0400

    changed section layout

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Sidebar from "../components/sidebar";
 import Head from "next/head";
-import Background from "../public/assets/craftbeer2-bg.png";
+import OlivesBg from "../public/assets/craftbeer2-sectionbg4.png";
+import Beer1 from "../public/assets/craftbeer2-subheader-pic1.png";
 
 export default function Home() {
   return (
@@ -15,15 +16,37 @@ export default function Home() {
           rel="stylesheet"
         />
       </Head>
-      <div className="home_container">
+      <div
+        style={{
+          background:
+            "url(" + `${require("../public/assets/craftbeer2-bg.png")}` + ")",
+        }}
+        className="home_container"
+      >
         <Sidebar />
-        <div
-          style={{
-            backgroundImage:
-              "url(" + `${require("../public/assets/craftbeer2-bg.png")}` + ")",
-          }}
-          className="home_content"
-        ></div>
+        <div className="home_content">
+          <img className="olive_bg" src={OlivesBg} alt="olives" />
+          <section className="section1">
+            <div className="home_info">
+              <div>
+                <h1>Brewed with passion and perfection</h1>
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet enim. Etiam ullamcorper non felis.
+                  Maecenas malesuada elit lectus felis laoreet.
+                </p>
+              </div>
+              <div>
+                <button>About Us</button>
+                <button>Our Beers</button>
+              </div>
+            </div>
+            <div className="home_img1">
+              <img src={Beer1} />
+            </div>
+          </section>
+        </div>
       </div>
     </>
   );
