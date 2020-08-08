a26f51051ecabf0b6ff683efa8a90eacddf6a9cf
jremy813 <jremywatkins@gmail.com>
2020-08-08 16:33:49 -0400

    added logo to header and got the toggle to work

--- components/header.js
+++ components/header.js
@@ -10,26 +10,27 @@ export default function Header() {
 
   return (
     <div className={styles.navbar}>
+      <img src="/assets/craftbeer2.png" />
       <nav>
         <div onClick={toggleNav} className={styles.hamburger}>
           <div className={styles.line}></div>
           <div className={styles.line}></div>
           <div className={styles.line}></div>
         </div>
+        {clicked && (
+          <ul className={styles.nav_links}>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        )}
       </nav>
-      {clicked && (
-        <ul className={styles.nav_links}>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-        </ul>
-      )}
     </div>
   );
 }
