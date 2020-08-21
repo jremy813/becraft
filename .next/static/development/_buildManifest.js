887ce89451ab7d16f4d01433eac2fc1426d9071d
jremy813 <jremywatkins@gmail.com>
2020-08-20 20:01:35 -0400

    added deskstop styling

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout.js"],"/beers":["static\u002Fchunks\u002Fpages\u002Fbeers.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/beers":["static\u002Fchunks\u002Fpages\u002Fbeers.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
